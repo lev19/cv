f3a698598a2f0ed3b5728e644783c4ae7b4dbac9
lev19 <levbio@yahoo.com>
2020-03-28 11:08:27 +0300

    Add Silverbyte images #3

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
-import { Popup } from 'semantic-ui-react';
+import { Popup, Grid ,Message, Segment} from 'semantic-ui-react';
 class Portfolio extends Component {
   render() {
 
     if (this.props.data) {
+      
+      const GridPorjectItems = (projects) =>(
+        <Segment>
+        <Message  info header={projects.projectTitle} content={projects.projectDescription} />
+        <Grid columns='equal' padded celled='internally'>
+           <Grid.Row  key={-1}>
+              <Grid.Column>TITLE</Grid.Column>
+              <Grid.Column>SKILLS</Grid.Column>
+            </Grid.Row>
+          {projects.itemsproject.map((item,index) => (
+            <Grid.Row  key={index}>
+              <Grid.Column>{item.title}</Grid.Column>
+              <Grid.Column>{item.skills}</Grid.Column>
+            </Grid.Row>
+          ))}
+        </Grid>
+        </Segment>
+      )
+
+      const imageRender = (projects) =>{return (
+        <a href={projects.url} target="_blank" title={projects.title}>
+          <img alt={projects.title} src={'images/portfolio/' + projects.image} />
+          <div className="overlay">
+            <div className="portfolio-item-meta">
+              <h5>{projects.title}</h5>
+              <p>{projects.category}</p>
+            </div>
+          </div>
+          <div className="link-icon"><i className="fa fa-link"></i></div>
+        </a>
+      )}
       var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = 'images/portfolio/' + projects.image;
+
         return <div key={projects.title} className="columns portfolio-item">
           <div className="item-wrap">
 
-            {!projects.url && <Popup
-              content='I will not flip!'
-              on='click'
-              pinned
-              trigger={<a title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
-                </div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
-              </a>}
-            />}
+            {!projects.url && 
+            <Popup on='click' wide trigger={imageRender(projects)}>
+              {GridPorjectItems(projects)}
+            </Popup>}
 
-            {projects.url && <a href={projects.url} target="_blank" title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                  <p>{projects.category}</p>
-                </div>
-              </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>}
+            {projects.url && imageRender(projects)}
 
           </div>
         </div>
