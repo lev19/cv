2c55ab9fc0d9457e29d65c19f6581891fb480553
lev19 <levbio@yahoo.com>
2020-03-29 04:06:29 +0300

    Improve pdf dawnload file #2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -36,7 +36,7 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="http://formspree.io/levbio@yahoo.com" method="post" id="contactForm" name="contactForm">
+               <form action="https://formspree.io/levbio@yahoo.com" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
